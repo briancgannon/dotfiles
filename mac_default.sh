6598db2c9efac174394015b1223870e14c90dbf2
Brian Gannon <briancgannon@gmail.com>
2019-02-18 09:21:11 -0500

    mac defaults

--- mac_default.sh
+++ mac_default.sh
@@ -1,4 +1,10 @@
-
+#/bin/bash
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
 
